ca2cc3713905e2550a8364b2f6c7dda78747c694
uhb5775 <58886255+uhb5775@users.noreply.github.com>
2021-02-07 23:10:05 +0200

    Update firebase.js

--- components/firebase.js
+++ components/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 
 const firebaseConfig = { 
-  apiKey: "AIzaSyASxG9JMzdsDHIL2EDTwdFDHJdQeWWWiNs",
-  authDomain: "hlm-map-c340f.firebaseapp.com",
-  databaseUrl: "https://hlm-map-c340f-default-rtdb.firebaseio.com/",
-  projectId: "hlm-map-c340f",
-  storageBucket: "hlm-map-c340f.appspot.com",
-  messagingSenderId: "937932607677",
-  appId: "1:937932607677:web:90a0e27ff1ca022481a7d4"
+  apiKey: "",
+  authDomain: "",
+  databaseUrl: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }; 
 
 if (firebase.apps.length === 0) {
